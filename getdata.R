32f793230305de208d12ca5ba69aec201d7c378c
Andrew B. Cooper <andrewbcooper@users.noreply.github.com>
2025-04-25 08:13:21 -0700

    initialcommit

---
+++ getdata.R
@@ -0,0 +1,16 @@
+
+library(readr)
+library(tidygeocoder)
+
+#mapboxdata <- finaldata %>% group_by(city,state,zip,geoPoint.lat,geoPoint.lon) %>% summarize(mycount=n())
+
+#write_rds(mapboxdata[1:10,],"mapboxdata")
+silly <- read_rds("mapboxdata")
+
+sillygeo = tryCatch(geo(address = unique(silly$zip),method="mapbox"),error=function(e) {
+  Sys.sleep(3)
+  geo(address = unique(silly$zip),method="mapbox")
+})
+
+
+write_rds(sillygeo,"mygeo")
